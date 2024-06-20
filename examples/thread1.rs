78729d8ca56b1556a1e4423920db147307f8dfd4
Mengting Xie <xmengt@gmail.com>
2024-06-19 22:19:51 -0400

    feature: first try on rust thread

---
+++ examples/thread1.rs
@@ -0,0 +1,60 @@
+use std::{sync::mpsc, thread};
+use std::time::Duration; 
+use anyhow::Result;
+
+const NUM_PRODUCERS: usize = 4;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Msg {
+    id: usize,
+    value: usize,
+}
+
+fn main() -> Result<()>{
+    let (tx, rx) = mpsc::channel();
+    
+    //producers
+    for i in 0..NUM_PRODUCERS {
+        let tx = tx.clone();
+        thread::spawn(move || producer(i, tx));
+    }
+    drop(tx);
+
+    //consumer
+    let consumer = thread::spawn(move || {
+        for msg in rx {
+            println!("consumer: {:?}", msg);
+        }
+        println!("consumer exit");
+        42
+    });
+
+    let secret = consumer.join().map_err(|e | anyhow::anyhow!("Thread join error: {:?}",e))?;
+    println!("secret: {:?}", secret);
+    // thread::sleep(Duration::from_secs(5));
+    Ok(())
+}
+
+fn producer(idx: usize, tx: mpsc::Sender<Msg>) -> Result<()>{
+    loop {
+        let value = rand::random::<usize>();
+        tx.send(Msg::new(idx,value))?;
+        let sleep_time = rand::random::<u64>() % 5;
+        thread::sleep(Duration::from_millis(sleep_time as _));
+        //random exit the producer
+        if rand::random::<u8>() % 10 == 0 {
+            println!("producer {} exit", idx);
+            break;
+        }
+    }
+    Ok(())
+}
+
+
+
+impl Msg {
+    fn new(id: usize, value: usize) -> Self {
+        Msg { id, value }
+    }
+}
